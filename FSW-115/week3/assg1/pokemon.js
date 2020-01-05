887883c6fb37ad81ca6faeb87d616647e348cafe
GiselleMartinez006 <53924501+GiselleMartinez006@users.noreply.github.com>
2020-01-04 23:00:58 -0500

    pokemons1

---
+++ FSW-115/week3/assg1/pokemon.js
@@ -0,0 +1,38 @@
+console.log("pokemon");
+// xhr.onreadystatechange
+// xhr.readyState
+// xhr.status
+// xhr.responseText
+
+// xhr.open()
+// xhr.send()
+
+// url: https://swapi.co/api/people/1
+
+const xhr = new XMLHttpRequest();
+// method  // url                     // async?
+xhr.open("GET", "https://api.vschool.io/pokemon", true);
+xhr.send();
+
+xhr.onreadystatechange = function() {
+  if (xhr.readyState === 4 && xhr.status === 200) {
+    const JSONdata = xhr.responseText;
+    const data = JSON.parse(JSONdata);
+    console.log(data.objects[0].pokemon);
+    showData(data.objects[0]);
+  } else if (xhr.readyState === 4 && xhr.status !== 200) {
+    console.log(xhr.responseText);
+  }
+};
+
+showData = data => {
+  for (i = 0; i < data.pokemon.length; i++) {
+    const section = document.createElement("section");
+    document.body.append(section);
+    const h1 = document.createElement("h1");
+    const h2 = document.createElement("h2");
+    h1.textContent = i + ") Name: " + data.pokemon[i].name;
+    h2.textContent = "URI: " + data.pokemon[i].resource_uri;
+    section.append(h1, h2);
+  }
+};
