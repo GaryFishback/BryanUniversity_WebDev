e566d2c8d030e2ac6602e56e6bdda8b093a90bf0
GiselleMartinez006 <53924501+GiselleMartinez006@users.noreply.github.com>
2020-01-20 00:17:51 -0500

    plumbing services imgs

--- FSW-120/Week5/mock_spa_pumbling/src/jsPerFunction/About.js
+++ FSW-120/Week5/mock_spa_pumbling/src/jsPerFunction/About.js
@@ -1,45 +1,71 @@
 import React, { Component } from "react";
-import AboutPoints from "./Paragraphs";
+// import AboutPoints from "./Paragraphs";
 class About extends Component {
   render() {
     //sp is for the span element in the Checkboxes function.
     return (
-      <div className="about">
-        <h3 style={{ fontSize: "30px" }}>Who and What?</h3>
-        <figure>
-          <img src=""></img>
-        </figure>
-        <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged. It was popularised in the 1960s with
-          the release of Letraset sheets containing Lorem Ipsum passages, and
-          more recently with desktop publishing software like Aldus PageMaker
-          including versions of Lorem Ipsum.
-        </p>
-        <h3 style={{ fontSize: "30px" }}>Why Us?</h3>
-        <figure>
-          <img src=""></img>
-        </figure>
-        <p>
-          It is a long established fact that a reader will be distracted by the
-          readable content of a page when looking at its layout. The point of
-          using Lorem Ipsum is that it has a more-or-less normal distribution of
-          letters, as opposed to using 'Content here, content here', making it
-          look like readable English. Many desktop publishing packages and web
-          page editors now use Lorem Ipsum as their default model text, and a
-          search for 'lorem ipsum' will uncover many web sites still in their
-          infancy. Various versions have evolved over the years, sometimes by
-          accident, sometimes on purpose (injected humour and the like).This is
-          a paragraph. This is the place to write a description about your
-          business or services. You can add and edit text. Hover over me and
-          click me once to show the pop-up menu. Double click me to edit the
-          text. You can also move me around on the page by drag and dropping me.
-          ​
-        </p>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column"
+        }}
+        className="about"
+      >
+        <section
+          style={{
+            display: "grid",
+            gridTemplateColumns: "50% 50%",
+            gridTemplateRows: "55px auto",
+            padding: " 50px",
+            paddingTop: "25px",
+            borderBottom: "solid 10px red"
+          }}
+        >
+          <h3 style={{ fontSize: "25px", width: "100%" }}>Who and What?</h3>
+          <figure>
+            <img
+              style={{ gridRow: "span 2" }}
+              src="https://raw.githubusercontent.com/GiselleMartinez006/BryanUniversity_WebDev/master/FSW-120/Week5/mock_spa_pumbling/src/imgs/download.jpg"
+            ></img>
+          </figure>
+          <p style={{ fontSize: "20px" }}>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged.
+          </p>
+        </section>
+        <section
+          style={{
+            display: "grid",
+            gridTemplateColumns: "50% 50%",
+            gridTemplateRows: "55px auto",
+            gridColumnGap: "40px",
+            padding: "50px",
+            // paddingRight: "50px",
+            height: "auto"
+          }}
+        >
+          <h3 style={{ fontSize: "25px", width: "100%" }}>Why Us?</h3>
+          {/* <figure> */}
+          <img
+            style={{ gridRow: "span 2" }}
+            src="https://raw.githubusercontent.com/GiselleMartinez006/BryanUniversity_WebDev/master/FSW-120/Week5/mock_spa_pumbling/src/imgs/FH15FEB_WKPLUMB_03.jpg"
+          ></img>
+          {/* </figure> */}
+          <p style={{ fontSize: "20px", marginRight: "15px" }}>
+            It is a long established fact that a reader will be distracted by
+            the readable content of a page when looking at its layout. The point
+            of using Lorem Ipsum is that it has a more-or-less normal
+            distribution of letters, as opposed to using 'Content here, content
+            here', making it look like readable English. Many desktop publishing
+            packages and web page editors now use Lorem Ipsum as their default
+            model text, and a search for 'lorem ipsum' will uncover many web
+            sites still in their infancy.
+          </p>
+        </section>
       </div>
     );
   }
--- FSW-120/Week5/mock_spa_pumbling/src/jsPerFunction/Services.js
+++ FSW-120/Week5/mock_spa_pumbling/src/jsPerFunction/Services.js
@@ -1,14 +1,36 @@
 import React, { Component } from "react";
 import Service from "./Paragraphs";
+var services = [
+  "Repair of sanitary devices",
+  "Uncorking",
+  "Search and repair water leak",
+  "Search and repair gaz leak",
+  "Search of breakdowns",
+  "Junction",
+  "Water connection",
+  "Gaz connection"
+];
+var imgs = [];
 class Services extends Component {
   render() {
     //sp is for the span element in the Checkboxes function.
     return (
       <div className="services">
-        <Service className="firstService " sp1="Service 1" />
-        <Service className="secondService " sp1="Service 2" />
-        <Service className="thirdService  " sp1="Service 3" />
-        <Service className="fourthService  " sp1="Service 4" />
+        {services.map(nums => {
+          return (
+            <section key={nums}>
+              <h2>{nums}</h2>
+              <p>
+                {" "}
+                Many desktop publishing packages and web page editors now use
+                Lorem Ipsum as their default model text, and a search for 'lorem
+                ipsum' will uncover many web sites still in their infancy.
+                Various versions have evolved over the years, sometimes by
+                accident, sometimes on purpose (injected humour and the like).
+              </p>
+            </section>
+          );
+        })}
       </div>
     );
   }
