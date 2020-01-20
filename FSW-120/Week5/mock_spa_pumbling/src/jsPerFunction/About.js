06c47ab8dd7ad9affe86b769496e975df32f76da
GiselleMartinez006 <53924501+GiselleMartinez006@users.noreply.github.com>
2020-01-19 21:11:26 -0500

    plumbing about imgs

--- FSW-120/Week5/mock_spa_pumbling/src/jsPerFunction/About.js
+++ FSW-120/Week5/mock_spa_pumbling/src/jsPerFunction/About.js
@@ -5,10 +5,41 @@ class About extends Component {
     //sp is for the span element in the Checkboxes function.
     return (
       <div className="about">
-        <AboutPoints className="firstAboutPoint" sp1="About Stuff Here" />
-        <AboutPoints className="secondAboutPoint" sp1="About Stuff 2" />
-        <AboutPoints className="thirdAboutPoint" sp1="About Stuff Here3" />
-        <AboutPoints className="fourthAboutPoint" sp1="About Stuff Here4" />
+        <h3 style={{ fontSize: "30px" }}>Who and What?</h3>
+        <figure>
+          <img src=""></img>
+        </figure>
+        <p>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged. It was popularised in the 1960s with
+          the release of Letraset sheets containing Lorem Ipsum passages, and
+          more recently with desktop publishing software like Aldus PageMaker
+          including versions of Lorem Ipsum.
+        </p>
+        <h3 style={{ fontSize: "30px" }}>Why Us?</h3>
+        <figure>
+          <img src=""></img>
+        </figure>
+        <p>
+          It is a long established fact that a reader will be distracted by the
+          readable content of a page when looking at its layout. The point of
+          using Lorem Ipsum is that it has a more-or-less normal distribution of
+          letters, as opposed to using 'Content here, content here', making it
+          look like readable English. Many desktop publishing packages and web
+          page editors now use Lorem Ipsum as their default model text, and a
+          search for 'lorem ipsum' will uncover many web sites still in their
+          infancy. Various versions have evolved over the years, sometimes by
+          accident, sometimes on purpose (injected humour and the like).This is
+          a paragraph. This is the place to write a description about your
+          business or services. You can add and edit text. Hover over me and
+          click me once to show the pop-up menu. Double click me to edit the
+          text. You can also move me around on the page by drag and dropping me.
+          ​
+        </p>
       </div>
     );
   }
--- FSW-120/Week5/mock_spa_pumbling/src/jsPerFunction/Home.js
+++ FSW-120/Week5/mock_spa_pumbling/src/jsPerFunction/Home.js
@@ -1,17 +1,42 @@
 import React, { Component } from "react";
-import Paragraphs from "./Paragraphs";
 class Home extends Component {
   render() {
     //sp is for the span element in the Welcome!es function.
     return (
       <div className="Home">
-        <figure>
-          <img src="https://raw.githubusercontent.com/GiselleMartinez006/BryanUniversity_WebDev/master/FSW-120/Week5/mock_spa_pumbling/src/imgs/SharedScreenshot.jpg"></img>
-        </figure>
-        <Paragraphs className="firstWelcome!" sp1="Welcome!" />
-        <Paragraphs className="secondWelcome!" sp1="Welcome! for Welcome! 2 " />
-        <Paragraphs className="thirdWelcome!" sp1="Welcome! 3" />
-        <Paragraphs className="fourthWelcome!" sp1="Welcome! 4" />
+        <section
+          style={{
+            display: "grid",
+            gridTemplateRows: "25% 25% 25% 25%",
+            gridTemplateColumns: "50% 50%"
+          }}
+        >
+          <figure style={{ gridRow: "span 4" }}>
+            <img src="https://raw.githubusercontent.com/GiselleMartinez006/BryanUniversity_WebDev/master/FSW-120/Week5/mock_spa_pumbling/src/imgs/Plumber-fi21004487x730.png"></img>
+          </figure>
+          <h1
+            style={{
+              gridRow: "span 2",
+              width: "100%",
+              fontSize: "75px",
+              color: "yellow"
+            }}
+          >
+            Got A Leak!?
+          </h1>
+          <h2
+            style={{
+              gridRow: "span 2",
+              width: "100%",
+              fontSize: "75px",
+              color: "red"
+            }}
+          >
+            We Got You!
+          </h2>
+        </section>
+        <section></section>
+        <section></section>
       </div>
     );
   }
