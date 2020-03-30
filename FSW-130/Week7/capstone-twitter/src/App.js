521f88e4f1fc18de9bef24ba05cd13a143764060
GiselleMartinez006 <53924501+GiselleMartinez006@users.noreply.github.com>
2020-03-30 18:39:20 -0400

    FSW-130

--- FSW-130/Week7/capstone-twitter/src/App.js
+++ FSW-130/Week7/capstone-twitter/src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import { /*Link,*/ Switch, Route } from "react-router-dom";
+import Login from "./jsPerFunction/LoginPage/Login";
+import LoggedIn from "./jsPerFunction/LoggedIn/LoggedIn";
+import LoggedOut from "./jsPerFunction/LoggedOut/LoggedOut";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./jsPerFunction/LoggedIn/redux/store";
+class App extends Component {
+  render() {
+    return (
+      <div
+        className="App"
+        style={{
+          display: "grid",
+          minHeight: " 100vh"
+        }}
+      >
+        <main>
+          <span></span>
+          <Switch>
+            <Route exact path="/" render={() => <Login />} />
+            <Route
+              path="/LoggedIn"
+              render={() => (
+                <Provider store={store}>
+                  <Router>
+                    <LoggedIn />
+                  </Router>
+                </Provider>
+              )}
+            />
+            <Route exact path="/LoggedOut" render={() => <LoggedOut />} />
+            {/* <Route path="/about" render={() => <About />} />
+            <Route path="/services" render={() => <Services />} /> */}
+          </Switch>
+          <span></span>
+        </main>
+      </div>
+    );
+  }
 }
-
 export default App;
