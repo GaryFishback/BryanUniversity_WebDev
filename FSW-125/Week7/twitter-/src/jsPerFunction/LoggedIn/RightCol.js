f03d7095362224f1c0f8b3145288f26417ff58d0
GiselleMartinez006 <53924501+GiselleMartinez006@users.noreply.github.com>
2020-03-31 13:21:22 -0400

    FSW-125

--- FSW-125/Week7/twitter-/src/jsPerFunction/LoggedIn/RightCol.js
+++ FSW-125/Week7/twitter-/src/jsPerFunction/LoggedIn/RightCol.js
@@ -1,16 +1,39 @@
 import React, { Component } from "react";
-
+import axios from "axios";
 class RightCol extends Component {
+  constructor() {
+    super();
+    this.state = { searchInput: "" };
+  }
   render() {
     return (
       <section>
         <form>
-          <svg viewBox="0 0 24 24">
-            <g>
-              <path d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z"></path>
-            </g>
-          </svg>
-          <input></input>
+          <a
+            href={`http://localhost:3030/tweets/search/name/?name=${this.state.searchInput}`}
+          >
+            <svg
+              viewBox="0 0 24 24"
+              onClick={() => {
+                axios
+                  .get(`tweets/search/name/?name=${this.state.searchInput}`)
+                  .then(res => {
+                    console.log(res.data);
+                    // this.props.dispatch(saveData(res.data.tweets));
+                  });
+              }}
+            >
+              <g>
+                <path d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z"></path>
+              </g>
+            </svg>
+          </a>
+          <input
+            onChange={e => {
+              console.log(e.target.value);
+              this.setState({ searchInput: e.target.value });
+            }}
+          ></input>
         </form>
         <h1>Trending now</h1>
         <h2>
