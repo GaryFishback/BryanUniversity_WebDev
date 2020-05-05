beb4535feef8e2235393d990e5996bdca0d637e7
GiselleMartinez006 <53924501+GiselleMartinez006@users.noreply.github.com>
2020-05-05 00:16:47 -0400

    FSW-140 queries

--- FSW-140/Week3/Anotehr.js
+++ FSW-140/Week3/Anotehr.js
@@ -22,7 +22,7 @@ var otherHeroes = [
     appearance: {
       gender: "Male",
       race: "Human",
-      height: [ "203 cm"],
+      height: ["203 cm"],
       weight: ["980 lb", "441 kg"],
       "eye-color": "Yellow",
       "hair-color": "No Hair",
@@ -58,7 +58,7 @@ var otherHeroes = [
     appearance: {
       gender: "Male",
       race: "Icthyo Sapien",
-      height: [ "191 cm"],
+      height: ["191 cm"],
       weight: ["145 lb", "65 kg"],
       "eye-color": "Blue",
       "hair-color": "No Hair",
@@ -94,7 +94,7 @@ var otherHeroes = [
     appearance: {
       gender: "Male",
       race: "Ungaran",
-      height: [ "185 cm"],
+      height: ["185 cm"],
       weight: ["200 lb", "90 kg"],
       "eye-color": "Blue",
       "hair-color": "No Hair",
@@ -131,7 +131,7 @@ var otherHeroes = [
     appearance: {
       gender: "Male",
       race: "Human / Radiation",
-      height: [  "203 cm"],
+      height: ["203 cm"],
       weight: ["980 lb", "441 kg"],
       "eye-color": "Green",
       "hair-color": "No Hair",
@@ -140,8 +140,7 @@ var otherHeroes = [
       occupation: "Ex-Spy",
       base: "Mobile",
     },
-    
-    
+
     image: {
       url: "https://www.superherodb.com/pictures2/portraits/10/100/1.jpg",
     },
@@ -178,8 +177,7 @@ var otherHeroes = [
       occupation: "Dimensional destroyer",
       base: "-",
     },
-    
-   
+
     image: {
       url: "https://www.superherodb.com/pictures2/portraits/10/100/181.jpg",
     },
@@ -214,7 +212,7 @@ var otherHeroes = [
     appearance: {
       gender: "Male",
       race: "Human",
-      height: [ "193 cm"],
+      height: ["193 cm"],
       weight: ["270 lb", "122 kg"],
       "eye-color": "Blue",
       "hair-color": "No Hair",
@@ -223,7 +221,7 @@ var otherHeroes = [
       occupation: "Professional criminal; former professional boxer",
       base: "-",
     },
-    
+
     image: {
       url: "https://www.superherodb.com/pictures2/portraits/10/100/1448.jpg",
     },
@@ -262,7 +260,7 @@ var otherHeroes = [
       base:
         "U.S.; formerly Diedenshausen, Germany; Milan, Italy; Paris, France; Japan; England; Atlanta, GA; Montreal, Canada",
     },
-   
+
     image: {
       url: "https://www.superherodb.com/pictures2/portraits/10/100/1026.jpg",
     },
@@ -290,7 +288,7 @@ var otherHeroes = [
     appearance: {
       gender: "Male",
       race: "Human",
-      height: [ "185 cm"],
+      height: ["185 cm"],
       weight: ["195 lb", "88 kg"],
       "eye-color": "Blue",
       "hair-color": "Blond",
@@ -299,8 +297,7 @@ var otherHeroes = [
       occupation: "Adventurer, archaelogist, ambassador",
       base: "Rann, Alpha Centauri System",
     },
-    
-   
+
     image: {
       url: "https://www.superherodb.com/pictures2/portraits/10/100/626.jpg",
     },
@@ -328,7 +325,7 @@ var otherHeroes = [
     appearance: {
       gender: "Female",
       race: "null",
-      height: [ "173 cm"],
+      height: ["173 cm"],
       weight: ["135 lb", "61 kg"],
       "eye-color": "Blue",
       "hair-color": "Blond",
@@ -337,7 +334,7 @@ var otherHeroes = [
       occupation: "Agent of SHIELD",
       base: "S.H.I.E.L.D. Headquarters, New York City",
     },
-    
+
     image: {
       url: "https://www.superherodb.com/pictures2/portraits/10/100/167.jpg",
     },
@@ -374,7 +371,7 @@ var otherHeroes = [
       occupation: "Mercenary, janitor; former pirate, terrorist",
       base: "Mobile; formerly Manhattan, Hellcarrier",
     },
-   
+
     image: {
       url: "https://www.superherodb.com/pictures2/portraits/10/100/10255.jpg",
     },
@@ -402,7 +399,7 @@ var otherHeroes = [
     appearance: {
       gender: "Male",
       race: "null",
-      height: [ "191 cm"],
+      height: ["191 cm"],
       weight: ["230 lb", "104 kg"],
       "eye-color": "-",
       "hair-color": "-",
@@ -411,7 +408,7 @@ var otherHeroes = [
       occupation: "Mercenary, former government operative, freedom fighter",
       base: "-",
     },
-    
+
     image: {
       url: "https://www.superherodb.com/pictures2/portraits/10/100/396.jpg",
     },
@@ -440,7 +437,7 @@ var otherHeroes = [
     appearance: {
       gender: "Male",
       race: "null",
-      height: [  "188 cm"],
+      height: ["188 cm"],
       weight: ["240 lb", "108 kg"],
       "eye-color": "Blue",
       "hair-color": "White",
@@ -449,7 +446,7 @@ var otherHeroes = [
       occupation: "Former starship captain, Herald of Galactus",
       base: "-",
     },
-    
+
     image: {
       url: "https://www.superherodb.com/pictures2/portraits/10/100/1358.jpg",
     },
@@ -477,7 +474,7 @@ var otherHeroes = [
     appearance: {
       gender: "Male",
       race: "Cyborg",
-      height: [ "193 cm"],
+      height: ["193 cm"],
       weight: ["200 lb", "90 kg"],
       "eye-color": "Brown",
       "hair-color": "Black",
@@ -486,7 +483,7 @@ var otherHeroes = [
       occupation: "-",
       base: "-",
     },
-    
+
     image: {
       url: "https://www.superherodb.com/pictures2/portraits/10/100/10422.jpg",
     },
@@ -514,7 +511,7 @@ var otherHeroes = [
     appearance: {
       gender: "Male",
       race: "null",
-      height: [ "180 cm"],
+      height: ["180 cm"],
       weight: ["201 lb", "90 kg"],
       "eye-color": "Blue",
       "hair-color": "Blond",
@@ -523,9 +520,7 @@ var otherHeroes = [
       occupation: "-",
       base: "Gotham City and New York City; Formerly Capitol City",
     },
-    
-     
-     
+
     image: {
       url: "https://www.superherodb.com/pictures2/portraits/10/100/698.jpg",
     },
@@ -562,41 +557,38 @@ var otherHeroes = [
       occupation: "-",
       base: "-",
     },
-    
+
     image: {
       url: "https://www.superherodb.com/pictures2/portraits/10/100/10398.jpg",
     },
   },
 ];
 // console.log(Object.values(otherHeroes[0]))
-function makingDataStringHEROES (id){
-var hero = []
-for (let i = 0; i < Object.values(otherHeroes[id]).length; i++) {
-//    console.log( typeof Object.values(otherHeroes[id-1])[i])
-typeof Object.values(otherHeroes[id])[i] == "number"?
-hero.push(Object.values(otherHeroes[id])[i]) :
-    typeof Object.values(otherHeroes[id])[i] !== "object"?
-    hero.push(Object.values(otherHeroes[id])[i]) : 
-    // console.log(objectEntries = (Object.entries(Object.values(otherHeroes[id-1])[i]).flat()));
-    // var powerstats = Object.entries(Object.values(otherHeroes[id-1])[i]).flat();
-hero.push(JSON.stringify(Object.values(otherHeroes[id])[i]))
-    
-}
-console.log(hero)
+function makingDataStringHEROES(id) {
+  var hero = [];
+  for (let i = 0; i < Object.values(otherHeroes[id]).length; i++) {
+    //    console.log( typeof Object.values(otherHeroes[id-1])[i])
+    typeof Object.values(otherHeroes[id])[i] == "number"
+      ? hero.push(Object.values(otherHeroes[id])[i])
+      : typeof Object.values(otherHeroes[id])[i] !== "object"
+      ? hero.push(Object.values(otherHeroes[id])[i])
+      : // console.log(objectEntries = (Object.entries(Object.values(otherHeroes[id-1])[i]).flat()));
+        // var powerstats = Object.entries(Object.values(otherHeroes[id-1])[i]).flat();
+        hero.push(JSON.stringify(Object.values(otherHeroes[id])[i]));
+  }
+  console.log(hero);
 }
 // for (let i = 0; i < 15 ; i++) {
 // makingDataStringHEROES(i)
 // }
-randomBooloean = []
+randomBooloean = [];
 for (let i = 0; i < 30; i++) {
-
-let random = Math.random()
-console.log(Math.random())
-random < 0.5 ? randomBooloean.push(1): randomBooloean.push(0)
+  let random = Math.random();
+  console.log(Math.random());
+  random < 0.5 ? randomBooloean.push(1) : randomBooloean.push(0);
 }
 
-console.log(randomBooloean)
-
+console.log(randomBooloean);
 
 //  ALTER TABLE public."MoreHeroes" DROP COLUMN id;
 //  	ALTER TABLE "MoreHeroes" DROP COLUMN name;
@@ -609,26 +601,23 @@ console.log(randomBooloean)
 //  	ALTER TABLE "MoreHeroes" DROP COLUMN imageUrl;
 //    ALTER TABLE "MoreHeroes" DROP COLUMN wanttojoin;
 
-	
 //  ALTER TABLE "MoreHeroes" ADD id SERIAL PRIMARY KEY;
 //  ALTER TABLE public."MoreHeroes"
-//      ADD COLUMN name varchar; 
+//      ADD COLUMN name varchar;
 //  ALTER TABLE public."MoreHeroes"
-//      ADD COLUMN fullname varchar; 
+//      ADD COLUMN fullname varchar;
 //  ALTER TABLE public."MoreHeroes"
-//      ADD COLUMN powerstats varchar; 
+//      ADD COLUMN powerstats varchar;
 //  ALTER TABLE public."MoreHeroes"
 //      ADD COLUMN gender varchar;
 //  ALTER TABLE public."MoreHeroes"
-//      ADD COLUMN biography varchar; 
+//      ADD COLUMN biography varchar;
 //  ALTER TABLE public."MoreHeroes"
-//      ADD COLUMN appearance varchar; 
+//      ADD COLUMN appearance varchar;
 //  ALTER TABLE public."MoreHeroes"
-//      ADD COLUMN work varchar; 
+//      ADD COLUMN work varchar;
 //  ALTER TABLE public."MoreHeroes"
-//      ADD COLUMN imageUrl varchar; 
-
-
+//      ADD COLUMN imageUrl varchar;
 
 // ran queries 1-15 rows twice to save time (since this is not for a real purpose)
 //  INSERT INTO "MoreHeroes"  (name, powerstats, biography, appearance, work, imageurl, wanttojoin)
@@ -638,7 +627,7 @@ console.log(randomBooloean)
 //    '{"gender":"Male","race":"Human","height":["203 cm"],"weight":["980 lb","441 kg"],"eye-color":"Yellow","hair-color":"No Hair"}',
 //    '{"occupation":"Musician, adventurer, author; formerly talk show host","base":"-"}',
 //    '{"url":"https://www.superherodb.com/pictures2/portraits/10/100/10060.jpg"}' , 1 );
-     
+
 //  INSERT INTO "MoreHeroes"  (name, powerstats, biography, appearance, work, imageurl, wanttojoin)
 //  VALUES ('Abe Sapien',
 //   '{"intelligence":"88","strength":"28","speed":"35","durability":"65","power":"100","combat":"85"}',
@@ -646,7 +635,7 @@ console.log(randomBooloean)
 //   '{"gender":"Male","race":"Icthyo Sapien","height":["191 cm"],"weight":["145 lb","65 kg"],"eye-color":"Blue","hair-color":"No Hair"}',
 //   '{"occupation":"Paranormal Investigator","base":"-"}',
 //   '{"url":"https://www.superherodb.com/pictures2/portraits/10/100/956.jpg"}' , 0);
-     
+
 //  INSERT INTO "MoreHeroes"  (name, powerstats, biography, appearance, work, imageurl, wanttojoin)
 //  VALUES ('Abin Sur',
 //   '{"intelligence":"50","strength":"90","speed":"53","durability":"64","power":"99","combat":"65"}',
@@ -654,7 +643,7 @@ console.log(randomBooloean)
 //   '{"gender":"Male","race":"Ungaran","height":["185 cm"],"weight":["200 lb","90 kg"],"eye-color":"Blue","hair-color":"No Hair"}',
 //   '{"occupation":"Green Lantern, former history professor","base":"Oa"}',
 //   '{"url":"https://www.superherodb.com/pictures2/portraits/10/100/1460.jpg"}'  , 1 );
-     
+
 //  INSERT INTO "MoreHeroes" (name,powerstats, biography, appearance, work, imageurl)
 //  VALUES ( 'Abomination',
 //   '{"intelligence":"63","strength":"80","speed":"53","durability":"90","power":"62","combat":"95"}',
@@ -662,15 +651,15 @@ console.log(randomBooloean)
 //   '{"gender":"Male","race":"Human / Radiation","height":["203 cm"],"weight":["980 lb","441 kg"],"eye-color":"Green","hair-color":"No Hair"}',
 //   '{"occupation":"Ex-Spy","base":"Mobile"}',
 //   '{"url":"https://www.superherodb.com/pictures2/portraits/10/100/1.jpg"}' , 0 );
-     
+
 //  INSERT INTO "MoreHeroes" (name, powerstats,biography, appearance, work, imageurl)
 //  VALUES ( 'Abraxas',
 //   '{"intelligence":"88","strength":"63","speed":"83","durability":"100","power":"100","combat":"55"}',
-//   '{"full-name":"Abraxas","alter-egos":"No alter egos found.","aliases":["-"],"place-of-birth":"Within Eternity","first-appearance":"Fantastic Four Annual #2001","publisher":"Marvel Comics","alignment":"bad"}',      
+//   '{"full-name":"Abraxas","alter-egos":"No alter egos found.","aliases":["-"],"place-of-birth":"Within Eternity","first-appearance":"Fantastic Four Annual #2001","publisher":"Marvel Comics","alignment":"bad"}',
 //   '{"gender":"Male","race":"Cosmic Entity","height":["-","0 cm"],"weight":["- lb","0 kg"],"eye-color":"Blue","hair-color":"Black"}',
 //   '{"occupation":"Dimensional destroyer","base":"-"}',
 //   '{"url":"https://www.superherodb.com/pictures2/portraits/10/100/181.jpg"}'  , 0);
-     
+
 //  INSERT INTO "MoreHeroes" (name, powerstats,  biography, appearance, work, imageurl)
 // VALUES ( 'Absorbing Man',
 //   '{"intelligence":"38","strength":"80","speed":"25","durability":"100","power":"98","combat":"64"}',
@@ -678,7 +667,7 @@ console.log(randomBooloean)
 //   '{"gender":"Male","race":"Human","height":["193 cm"],"weight":["270 lb","122 kg"],"eye-color":"Blue","hair-color":"No Hair"}',
 //   '{"occupation":"Professional criminal; former professional boxer","base":"-"}',
 //   '{"url":"https://www.superherodb.com/pictures2/portraits/10/100/1448.jpg"}'  , 1 );
-     
+
 //  INSERT INTO "MoreHeroes"  (name, powerstats, biography, appearance, work, imageurl, wanttojoin)
 //  VALUES ( 'Adam Monroe',
 //   '{"intelligence":"63","strength":"10","speed":"12","durability":"100","power":"100","combat":"64"}',
@@ -686,7 +675,7 @@ console.log(randomBooloean)
 //   '{"gender":"Male","race":"null","height":["-","0 cm"],"weight":["- lb","0 kg"],"eye-color":"Blue","hair-color":"Blond"}',
 //   '{"occupation":"former samurai swordsman, former British mercenary commander, former soldier in the Confederate Army","base":"U.S.; formerly Diedenshausen, Germany; Milan, Italy; Paris, France; Japan; England; Atlanta, GA; Montreal, Canada"}',
 //   '{"url":"https://www.superherodb.com/pictures2/portraits/10/100/1026.jpg"}'  , 0 );
-     
+
 //  INSERT INTO "MoreHeroes"  (name, powerstats, biography, appearance, work, imageurl, wanttojoin)
 //  VALUES ( 'Adam Strange',
 //   '{"intelligence":"69","strength":"10","speed":"33","durability":"40","power":"37","combat":"50"}',
@@ -694,7 +683,7 @@ console.log(randomBooloean)
 //   '{"gender":"Male","race":"Human","height":["185 cm"],"weight":["195 lb","88 kg"],"eye-color":"Blue","hair-color":"Blond"}',
 //   '{"occupation":"Adventurer, archaelogist, ambassador","base":"Rann, Alpha Centauri System"}',
 //   '{"url":"https://www.superherodb.com/pictures2/portraits/10/100/626.jpg"}'  , 0 );
-     
+
 //  INSERT INTO "MoreHeroes"  (name, powerstats, biography, appearance, work, imageurl, wanttojoin)
 //  VALUES ( 'Agent 13',
 //   '{"intelligence":"null","strength":"46","speed":"null","durability":"null","power":"null","combat":"null"}',
@@ -702,7 +691,7 @@ console.log(randomBooloean)
 //   '{"gender":"Female","race":"null","height":["173 cm"],"weight":["135 lb","61 kg"],"eye-color":"Blue","hair-color":"Blond"}',
 //   '{"occupation":"Agent of SHIELD","base":"S.H.I.E.L.D. Headquarters, New York City"}',
 //   '{"url":"https://www.superherodb.com/pictures2/portraits/10/100/167.jpg"}'  , 1 );
-     
+
 //  INSERT INTO "MoreHeroes"  (name, powerstats, biography, appearance, work, imageurl, wanttojoin)
 //  VALUES (  'Agent Bob',
 //   '{"intelligence":"10","strength":"8","speed":"13","durability":"5","power":"5","combat":"20"}',
@@ -710,7 +699,7 @@ console.log(randomBooloean)
 //   '{"gender":"Male","race":"Human","height":["178 cm"],"weight":["181 lb","81 kg"],"eye-color":"Brown","hair-color":"Brown"}',
 //   '{"occupation":"Mercenary, janitor; former pirate, terrorist","base":"Mobile; formerly Manhattan, Hellcarrier"}',
 //   '{"url":"https://www.superherodb.com/pictures2/portraits/10/100/10255.jpg"}'  , 0);
-     
+
 //  INSERT INTO "MoreHeroes"  (name, powerstats, biography, appearance, work, imageurl, wanttojoin)
 //  VALUES ( 'Agent Zero',
 //   '{"intelligence":"75","strength":"28","speed":"38","durability":"80","power":"72","combat":"95"}',
@@ -718,7 +707,7 @@ console.log(randomBooloean)
 //   '{"gender":"Male","race":"null","height":["191 cm"],"weight":["230 lb","104 kg"],"eye-color":"-","hair-color":"-"}',
 //   '{"occupation":"Mercenary, former government operative, freedom fighter","base":"-"}',
 //   '{"url":"https://www.superherodb.com/pictures2/portraits/10/100/396.jpg"}'  , 0 );
-     
+
 //  INSERT INTO "MoreHeroes"  (name, powerstats, biography, appearance, work, imageurl, wanttojoin)
 //  VALUES ( 'Air-Walker',
 //   '{"intelligence":"50","strength":"85","speed":"100","durability":"85","power":"100","combat":"40"}',
@@ -726,7 +715,7 @@ console.log(randomBooloean)
 //   '{"gender":"Male","race":"null","height":["188 cm"],"weight":["240 lb","108 kg"],"eye-color":"Blue","hair-color":"White"}',
 //   '{"occupation":"Former starship captain, Herald of Galactus","base":"-"}',
 //   '{"url":"https://www.superherodb.com/pictures2/portraits/10/100/1358.jpg"}' , 1 );
-     
+
 //  INSERT INTO "MoreHeroes"  (name, powerstats, biography, appearance, work, imageurl, wanttojoin)
 //  VALUES ( 'Ajax',
 //   '{"intelligence":"56","strength":"48","speed":"35","durability":"80","power":"34","combat":"55"}',
@@ -734,14 +723,14 @@ console.log(randomBooloean)
 //   '{"gender":"Male","race":"Cyborg","height":["193 cm"],"weight":["200 lb","90 kg"],"eye-color":"Brown","hair-color":"Black"}',
 //   '{"occupation":"-","base":"-"}',
 //   '{"url":"https://www.superherodb.com/pictures2/portraits/10/100/10422.jpg"}'  , 0 );
-     
+
 //  INSERT INTO "MoreHeroes"  (name, powerstats, biography, appearance, work, imageurl, wanttojoin)
 //  VALUES ( 'Alan Scott',
 //   '{"intelligence":"63","strength":"80","speed":"23","durability":"90","power":"100","combat":"32"}',
 //   '{"full-name":"Alan Ladd Wellington Scott","alter-egos":"No alter egos found.","aliases":["Green Lantern","White King","Sentinal"],"place-of-birth":"Gotham City","first-appearance":"All-American Comics 16 (1940)","Green Lantern","White King","Sentinal"],"place-of-birth":"Gotham City","first-appearance":"All-American Comics 16 (1940)","publisher":"DC Comics","alignment":"good"}',  '{"gender":"Male","race":"null","height":["180 cm"],"weight":["201 lb","90 kg"],"eye-color"r":"Blue","hair-color":"Blond"}',
-//   '{"occupation":"-","base":"Gotham City and New York City; Formerly Capitol City"}',        
+//   '{"occupation":"-","base":"Gotham City and New York City; Formerly Capitol City"}',
 //   '{"url":"https://www.superherodb.com/pictures2/portraits/10/100/698.jpg"}'  , 1 );
-     
+
 //  INSERT INTO "MoreHeroes"  (name, powerstats, biography, appearance, work, imageurl, wanttojoin)
 //  VALUES ( 'Alex Mercer',
 //   '{"intelligence":"50","strength":"80","speed":"42","durability":"90","power":"100","combat":"50"}',
@@ -749,8 +738,6 @@ console.log(randomBooloean)
 //   '{"gender":"Male","race":"Human","height":["-","0 cm"],"weight":["- lb","0 kg"],"eye-color":"-","hair-color":"-"}',
 //   '{"occupation":"-","base":"-"}',
 //   '{"url":"https://www.superherodb.com/pictures2/portraits/10/100/10398.jpg"}', 1 );
-     
-     
 
 // WHEN 1 THEN 1
 // WHEN 2 THEN 1
@@ -782,22 +769,20 @@ console.log(randomBooloean)
 //  WHEN 28 THEN  1
 //  WHEN 29 THEN  0
 //   WHEN 30 THEN 1
- 
 
-
-CREATE TABLE frames (framed INT,
-   apple INT,
-   circle INT,
-   double_oval INT,
-   florida INT,
-   half_circle INT,
-   half_oval INT,
-   oval INT,
-   rectangle_3d INT,
-   rectangular INT,
-   seashell INT,
-   split INT,
-   tomb INT,
-   triple INT,
-   window_frame INT,
-   wood INT);
+// CREATE TABLE frames (framed INT,
+//    apple INT,
+//    circle INT,
+//    double_oval INT,
+//    florida INT,
+//    half_circle INT,
+//    half_oval INT,
+//    oval INT,
+//    rectangle_3d INT,
+//    rectangular INT,
+//    seashell INT,
+//    split INT,
+//    tomb INT,
+//    triple INT,
+//    window_frame INT,
+//    wood INT);
